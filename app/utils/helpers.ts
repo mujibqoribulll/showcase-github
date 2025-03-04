d6aa3bb533585c5b4d071e7c52058d2bdf530306
qoribulmujib <muhamadqoribulmujib@gmail.com>
2025-03-05 01:51:28 +0700

    🔥 💄 create function helpers

---
+++ app/utils/helpers.ts
@@ -0,0 +1,39 @@
+import InitialState, { ResetStatusHook } from "../store/types";
+
+export const resetStatusHook = (
+    initialState: InitialState,
+    state: InitialState,
+    key: ResetStatusHook,
+  ) => {
+    let stateNew = {...state};
+    switch (key) {
+      case 'loading':
+        stateNew.loading = 'idle';
+        stateNew.message = '';
+        break;
+      case 'data':
+        stateNew.data = {};
+        break;
+      case 'all':
+        stateNew = initialState;
+        break;
+      default:
+        break;
+    }
+    return stateNew;
+  };
+
+
+  export const setErrorMessage = (action: any) => {
+    if (typeof action === 'string') return action;
+    let error = action?.paylaod || action;
+    let message =
+      error?.response?.data?.data?.message ??
+      error?.response?.data?.meta?.message ??
+      error?.response?.data?.message ??
+      error?.response?.message ??
+      error?.message ??
+      'Server Sedang Mengalami Gangguan';
+  
+    return message;
+  };
